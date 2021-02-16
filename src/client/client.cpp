ebdfdbae419bf1d4c2fde7eff451a716a3396021
da3m0nsec <alvifr@hotmail.com>
2021-02-16 18:40:20 +0000

    Add travis-CI

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -49,7 +49,7 @@ int main()
         serv_addr.sin_port = htons(PORT);
 
         // Convert IPv4 and IPv6 addresses from text to binary form
-        if (inet_pton(AF_INET, "192.168.1.41", &serv_addr.sin_addr) <= 0)
+        if (inet_pton(AF_INET, "192.168.1.47", &serv_addr.sin_addr) <= 0)
         {
             printf("\nInvalid address/ Address not supported \n");
             return 2;
@@ -65,11 +65,6 @@ int main()
         std::cout << "Sending msg" << std::endl;
         send(sock, &msg, sizeof(msg), 0);
         sleep(20);
-
-        /*
-        valread = read( sock , buffer, 1024); 
-        printf("%s\n",buffer ); 
-        */
     }
     return 0;
 }
